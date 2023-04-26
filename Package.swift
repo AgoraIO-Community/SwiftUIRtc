a8af218932b487f4731f94dddcc92d46d9899311
Cristian Petra <petra.cristian@gmail.com>
2023-04-26 12:08:15 +0300

    upd: Update minimum version to 14

--- Package.swift
+++ Package.swift
@@ -5,7 +5,7 @@ import PackageDescription
 
 let package = Package(
     name: "AgoraVideoSwiftUI",
-    platforms: [.iOS(.v13)],
+    platforms: [.iOS(.v14)],
     products: [
         .library(name: "AgoraVideoSwiftUI", targets: ["AgoraVideoSwiftUI"]),
     ],
